c8d900d102483c7c61d5bb9befb13c22a1691614
Eric Hotinger <ehotinger@gmail.com>
2018-06-28 00:56:40 -0700

    init

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func main() {
+	fmt.Println("Listening on :8080")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
